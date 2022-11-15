095631a91e47d5e2b5de921c3a42d9f2739bdfc5
kmingx <denebolx@gmail.com>
2022-11-15 14:29:34 +0900

    대중 메뉴 및 인코딩 오류 수정

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -4,7 +4,10 @@ from django.shortcuts import render
 from django.shortcuts import render
 from .models import Question
 from .models import Menu
+
 from django.db.models import Q
+
+from . import load
 
 
 def index(request):
@@ -19,7 +22,14 @@ def indexpage(request):
     return render(request, 'pybo/index.html')
 
 def menuList(request):
+    #files_Path = load.LoadConfig.new_file()
+    inputaudioPath = load.LoadConfig.audioFileLoad()
+    #print(inputaudioPath)
+    mask1 = load.LoadConfig.model.predict(inputaudioPath)
+    print(mask1) # CLI 분류값 확인
+    peopleType = load.LoadConfig.classification(mask1)
 
+    #print(mask1)
     kw = request.POST.get('kw','') # 값
     audiourl = request.POST.get('audiourl','') # 값
     audiourl2 = request.POST.get('audiourl2','') # 값
@@ -31,23 +41,25 @@ def menuList(request):
 
     if kw:
         for i in kwList:
-            print(i)
+            #print(i)
             menu_list = Menu.objects.order_by('id')
             menu_list = menu_list.filter(
                 Q(menuName__icontains=i) |
                 Q(Price__icontains=i)
                 # Q(content__icontains=kw)
             ).distinct()
-            print(type(menu_list))
+            #print(type(menu_list))
             kwlist2.append(menu_list)
         #print("저장값 : ",kwlist2[0])
         #print("저장값 : ",kwlist2[1])
         for e in range(len(kwList)):
-            menu_list = kwlist2[0] | kwlist2[e]
-            #menu_list12 =  menu_list1 | menu_list12
-            #menu_list = kwlist2[0]|kwlist2[1]
+            try:
+                kwlist2[0] = kwlist2[0] | kwlist2[e]
+                menu_list = kwlist2[0]
+            except IndexError as e:
+                print("오류")
         #print(result_set2)
-        context = {'menu_list':menu_list,'kw': kw}
+        context = {'menu_list':menu_list,'kw': kw, 'peopleType':peopleType}
 
     return render(request, 'pybo/menu_list.html', context)
 
